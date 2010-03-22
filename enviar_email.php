d7912c4cde6c50aa4a80b2e87e57085484ede798
aloisio <aloisio@elianapedrosa.com.br>
2010-03-22 20:25:09 -0300

    mensagens humanizadas para usuarios

--- enviar_email.php
+++ enviar_email.php
@@ -1,12 +1,33 @@
-<?php
-	$destinatario= 'aloisio@elianapedrosa.com.br';
-	$assunto=  'Assunto: Teste';
-	$corpo= ' Apenas 1 test.';
-	mail($destinatario, $assunto, $corpo);
-
-	$assunto= $_POST['Assunto'];
-	$corpo= $_POST['Mensangem'];
-
-?>
-
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Kata Email</title>
+	<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<style> article, aside, figure, footer, header, hgroup, menu, nav, section { display: block; } </style>
 	
+</head>
+<body>
+	<div class="container">
+		
+			<?php
+				error_reporting(0);
+				$destinatario= 'aloisio@elianapedrosa.com.br';
+				$assunto= $_POST['Assunto'];
+				$corpo= $_POST['Mensangem'];
+	
+				if(mail($destinatario, $assunto, $corpo)):
+	
+			?>
+				<p>Seu email foi enviado. Obrigado.</p>
+	
+			<?php else: ?>
+	
+				<p>Não foi possível enviar seu email. Tente novamente mais tarde.</p>
+	
+			<?php endif; ?>
+			
+			
+	</div>
+</body>
+</html>
